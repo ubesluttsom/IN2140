38e3c9df59aa47d7cf90edaa260bdcad50e533f3
Martin Mihle Nygaard <martin@mihle.no>
2021-01-28 12:15:16 +0100

    Påbegynt oppgave 2, makefile linker

---
+++ oppgave2.c
@@ -0,0 +1,4 @@
+int stringsum (char* s) { return 0; }
+int distance_between (char* s, char c) { return 0; }
+char* string_between (char* s, char c) { return 0; }
+void stringsum2 (char* s, int* res) { return; }
